ae4c6e40a1414a0945937a34a3d6dfe79bdeefee
Zinam Mohamed <pk.x3311@gmail.com>
2024-02-02 00:56:45 +0300

    use local stoarge to save tasks

--- script.js
+++ script.js
@@ -15,14 +15,28 @@ function addTask(){
         let task = document.createElement("li");
         task.innerHTML = `${input.value}<img src='./images/cancel 2.png'>`;
         lists.appendChild(task);
-        input.value = '';
     }
+    input.value = '';
+    storeTasks();
 }
 // event when clisk on one task to make it checked task
 lists.addEventListener('click',(e)=>{
     if(e.target.tagName==='LI'){
-        e.target.classList.toggle('checked')
+        e.target.classList.toggle('checked');
+        storeTasks();
     }else if(e.target.tagName === 'IMG'){
         e.target.parentElement.remove();
+        storeTasks();
     }
 },false)
+
+// to store the exist tasks in local stoarge
+function storeTasks(){
+    localStorage.setItem('tasks',lists.innerHTML);
+}
+
+// to retrivr tasks that stored in local stoarge and show them
+function showTasks(){
+    lists.innerHTML = localStorage.getItem('tasks');
+}
+showTasks();
